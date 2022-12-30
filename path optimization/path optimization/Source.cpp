60ceeffeda3904edf5f100c65d1b7a38b37adbf0
JoshCircenis <98178221+JoshCircenis@users.noreply.github.com>
2022-12-30 14:18:03 -0700

    Final working optimization algorithm
    This algorithm will choose the most efficient direction to move around a circle to replicate the desired angle given two inputs. This allows for better and smoother swerve drive because it can travel around the circle past the sensor transitions and can reverse the motor direction so that the max distance moved is only 90 degrees unless spinning while moving forward in a "tornado" maneuver.

--- path optimization/path optimization/Source.cpp
+++ path optimization/path optimization/Source.cpp
@@ -5,17 +5,25 @@ using namespace std;
 int main() {
 	int desired;
 	int current;
+	int direction = 1;
+
+
+
 	cout << "Enter current angle: ";
 	cin >> current;
 	cout << endl;
 	cout << "Enter desired angle: ";
 	cin >> desired;
 	cout << endl;
+
+
+
 	int angle1 = (desired - (current % 2048));
 	int angle2 = ((2048 - abs(desired % 2048)) + (2048 - abs(current % 2048)));
 	int shortest;
 	int check1;
-	cout << "Angle 1: " << angle1 << " Angle 2: " << angle2 << endl;
+	
+
 	if (abs(angle2) < abs(angle1)) {
 		shortest = angle2;
 		check1 = true;
@@ -27,7 +35,8 @@ int main() {
 
 	angle1 = ((desired - 2048) - (current % 2048));
 	angle2 = ((2048 - (abs(desired % 2048)-2048)) + (2048 - (abs(current % 2048))));
-	cout << "Angle 12: " << angle1 << " Angle 22: " << angle2 << endl;
+	
+
 	int shortest2;
 	bool check2;
 	if (abs(angle2) < abs(angle1)) {
@@ -39,7 +48,8 @@ int main() {
 		check2 = false;
 	}
 
-	if (abs(shortest) < abs(shortest2)) {
+
+	if (abs(shortest) <= abs(shortest2)) {
 		if (check1 == true) {
 			if (current < 0) {
 				current -= shortest;
@@ -53,6 +63,7 @@ int main() {
 		}
 	}
 	else {
+		direction = -1;
 		if (check2 == true) {
 			if (current < 0) {
 				current -= shortest2;
@@ -66,7 +77,7 @@ int main() {
 		}
 	}
 
-	cout << "Final angle: " << current << endl;
+	cout << "Final angle: " << current << " Direction: " << direction << endl;
 	system("pause");
 	system("CLS");
 	main();
